5d2c66dd7d8b71e0712564a39ba508b75537f18e
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-08-08 01:01:24 +0700

    drm: atomic: Don't allow boost CPU and DDR after input timeout
    * This prevents unnecessary boosts. for example when
      watching videos or other touchless actions.
    
    * Set the input timeout after 3250 ms or 3.2s so as not to
      do a useless boost, this is useful for saving power.
    
    * This change was inspired by commit https://github.com/caf-ginkgo/kernel_msm-4.14/commit/4ccf7265c0273f01c7fda6ba332ea78209bf9194,
      but we've adapted it to the existing changes.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Shashank Patil <9945shashank@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2251,7 +2251,8 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 		return -EINVAL;
 
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
-		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+		if (df_boost_within_input(3250))
+			devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 	}
 
 	drm_modeset_acquire_init(&ctx, 0);
